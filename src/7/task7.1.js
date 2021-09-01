c2dfef03d2bdf42de22a54a08ac74b8fdd0d1c30
Roman Likhomanov <romanlee87@gmail.com>
2021-09-01 11:44:47 +0300

    Error correction@5

--- src/7/task7.1.js
+++ src/7/task7.1.js
@@ -2,11 +2,16 @@ export function pageLayout(el) {
   const form = document.createElement("form");
   el.appendChild(form);
 
+  const button = document.createElement("button");
+  form.appendChild(button);
+  button.hidden = true;
+
   const input = document.createElement("input");
   form.appendChild(input);
 
-  const button = document.createElement("button");
-  form.appendChild(button);
+  input.addEventListener("keydown", () => {
+    button.hidden = false;
+  });
 
   const ulHistory = document.createElement("ul");
   form.appendChild(ulHistory);
@@ -23,4 +28,11 @@ export function pageLayout(el) {
   const li3 = document.createElement("li");
   ulHistory.appendChild(li3);
   li3.innerHTML = "I love world!";
+
+  form.addEventListener("submit", () => {
+    const li4 = document.createElement("li");
+    ulHistory.appendChild(li4);
+    li4.innerHTML = input.value;
+    input.value = "";
+  });
 }
--- src/7/task7.1.test.js
+++ src/7/task7.1.test.js
@@ -21,4 +21,35 @@ describe("pageLayout", () => {
       [...el.querySelectorAll("ul.history li")].map((elem) => elem.innerHTML)
     ).toEqual(values.map(([value]) => value));
   });
+
+  it("button is hidden", () => {
+    const el = document.createElement("div");
+    pageLayout(el);
+
+    const form = el.querySelector("form");
+    const button = form.querySelector("button");
+    expect(button.hidden).toBe(true);
+  });
+
+  it("show the button when entering", () => {
+    const el = document.createElement("div");
+    pageLayout(el);
+
+    const button = el.querySelector("button");
+    const input = el.querySelector("input");
+    input.dispatchEvent(new Event("keydown"));
+    expect(button.hidden).toBe(false);
+  });
+
+  it("text from the input to the list", () => {
+    const el = document.createElement("div");
+    pageLayout(el);
+
+    el.querySelector("input").value = "some text";
+    const form = el.querySelector("form");
+    form.dispatchEvent(new Event("submit"));
+    expect(el.querySelector("ul.history > li:last-child").innerHTML).toBe(
+      "some text"
+    );
+  });
 });
