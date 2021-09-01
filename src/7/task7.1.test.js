8043a886499f738f672217d18b9392efaa3159bb
Roman Likhomanov <romanlee87@gmail.com>
2021-09-01 15:17:37 +0300

    Error correction@6

--- src/7/task7.1.test.js
+++ src/7/task7.1.test.js
@@ -1,55 +1,52 @@
 import { pageLayout } from "./task7.1";
 
 describe("pageLayout", () => {
-  it("renders initial markup", () => {
-    const el = document.createElement("div");
-    pageLayout(el);
+  describe("functional tests", () => {
+    let el;
+    beforeEach(() => {
+      el = document.createElement("div");
+      pageLayout(el);
+    });
 
-    const form = el.querySelector("form");
-    expect(form).toBeTruthy();
-    expect(form.querySelector("input")).toBeTruthy();
-    expect(form.querySelector("button")).toBeTruthy();
-    expect(el.querySelector("ul.history")).toBeTruthy();
-  });
+    it("renders initial markup", () => {
+      const form = el.querySelector("form");
+      expect(form).toBeTruthy();
+      expect(form.querySelector("input")).toBeTruthy();
+      expect(form.querySelector("button")).toBeTruthy();
+      expect(el.querySelector("ul.history")).toBeTruthy();
+    });
 
-  it("renders history elements", () => {
-    const el = document.createElement("div");
-    pageLayout(el);
+    it("renders history elements", () => {
+      const values = [
+        ["I love JavaScript!"],
+        ["I love IT!"],
+        ["I love world!"],
+      ];
+      expect(
+        [...el.querySelectorAll("ul.history li")].map((elem) => elem.innerHTML)
+      ).toEqual(values.map(([value]) => value));
+    });
 
-    const values = [["I love JavaScript!"], ["I love IT!"], ["I love world!"]];
-    expect(
-      [...el.querySelectorAll("ul.history li")].map((elem) => elem.innerHTML)
-    ).toEqual(values.map(([value]) => value));
-  });
+    it("button is hidden", () => {
+      const form = el.querySelector("form");
+      const button = form.querySelector("button");
+      expect(button.hidden).toBe(true);
+    });
 
-  it("button is hidden", () => {
-    const el = document.createElement("div");
-    pageLayout(el);
+    it("show the button when entering", () => {
+      const button = el.querySelector("button");
+      const input = el.querySelector("input");
+      input.dispatchEvent(new Event("keydown"));
+      expect(button.hidden).toBe(false);
+    });
 
-    const form = el.querySelector("form");
-    const button = form.querySelector("button");
-    expect(button.hidden).toBe(true);
-  });
-
-  it("show the button when entering", () => {
-    const el = document.createElement("div");
-    pageLayout(el);
-
-    const button = el.querySelector("button");
-    const input = el.querySelector("input");
-    input.dispatchEvent(new Event("keydown"));
-    expect(button.hidden).toBe(false);
-  });
-
-  it("text from the input to the list", () => {
-    const el = document.createElement("div");
-    pageLayout(el);
-
-    el.querySelector("input").value = "some text";
-    const form = el.querySelector("form");
-    form.dispatchEvent(new Event("submit"));
-    expect(el.querySelector("ul.history > li:last-child").innerHTML).toBe(
-      "some text"
-    );
+    it("text from the input to the list", () => {
+      el.querySelector("input").value = "some text";
+      const form = el.querySelector("form");
+      form.dispatchEvent(new Event("submit"));
+      expect(el.querySelector("ul.history > li:last-child").innerHTML).toBe(
+        "some text"
+      );
+    });
   });
 });
